d5d9c50e5f11ad681e3e94687a0b73b8b22fa917
InMyCountry <553099157@qq.com>
2019-09-20 16:16:31 +0800

    添加Nlog 配置

--- StepByStepDemo/Controllers/StudentTestController.cs
+++ StepByStepDemo/Controllers/StudentTestController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Repository.IServices.IServices;
 using DBEntityModule.Test;
+using Microsoft.Extensions.Logging;
 
 namespace StepByStepDemo.Controllers
 {
@@ -15,12 +16,15 @@ namespace StepByStepDemo.Controllers
     {
         private readonly IStudentTestService _studentTestService;
         private readonly IUnitOfWork  _unitOfWork;
+        private readonly ILogger<StudentTestController> _logger;
 
-        public StudentTestController(IStudentTestService studentTestService, IUnitOfWork unitOfWork)
+        public StudentTestController(IStudentTestService studentTestService, IUnitOfWork unitOfWork, ILogger<StudentTestController> logger)
         {
             _studentTestService = studentTestService;
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
+
         [HttpGet]
         public IActionResult AddStudent()
         {
@@ -28,13 +32,21 @@ namespace StepByStepDemo.Controllers
             {
                 StudentTest studentTest = new StudentTest() { Id = Guid.NewGuid().ToString(), Age = 10, Gender = "boy", Name = "张三" };
                 _studentTestService.Add(studentTest);
-                _unitOfWork.Commit();
+               int num= _unitOfWork.Commit();
+                _logger.LogInformation($"添加{num}条数据");
+               
                 return Ok();
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
+        [HttpGet]
+        public IActionResult CreateLog()
+        {
+            _logger.LogError("123");
+            return Ok();
         }
     }
 }
--- StepByStepDemo/Program.cs
+++ StepByStepDemo/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace StepByStepDemo
 {
@@ -19,6 +20,12 @@ namespace StepByStepDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); //移除已经注册的其他日志处理程序
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace); //设置最小的日志级别
+                })
+                .UseNLog();
     }
 }
